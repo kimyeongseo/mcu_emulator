6709376f63c1c9e7c4bbe95a026a3d9043047b0b
김영서 <79343830+kimyeongseo@users.noreply.github.com>
2021-10-30 15:36:43 +0900

    Update Memory.java

--- Computer/src/Memory.java
+++ Computer/src/Memory.java
@@ -1,42 +1,39 @@
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Memory {
-	private short memory[] = new short[256];
-
-	int[] header = {};
-	int[] dataSegment = {};
-	int[] codeSegment = {};
 	
-	public void read() throws IOException{
-	  List<String> ret = fileLineRead("C:/Users/USER/Desktop/java/Computer/exe/exe");
-		for(int i = 0; i<ret.size(); i++){
-		  System.out.println(ret.get(i));
+	private short memory[];
+	
+
+	public Memory(){
+		this.memory = new short[512];
 		}
-	}
-
-	public static List<String> fileLineRead(String name) throws IOException{
-		List<String> retStr = new ArrayList<String>();
-	   BufferedReader in = new BufferedReader(new FileReader(name));
-		String s;
-		  while ((s = in.readLine()) != null) {
-			   retStr.add(s);
-		  }
-		in.close();
-		return retStr;  
-	 }
-
+	
 	public short load(short mar) {
 		return this.memory[mar];
 		
 	}
-
-	public void store(short mar, short mbr){ 
+	public void store(short mar, short mbr){ 				
 		this.memory[mar] = mbr;
 	}
- 
+
+	
+	public void readExe(String fileName) {
+		try {
+			Scanner scanner = new Scanner(new File("exe/"+fileName));
+			short address = 0;
+			while(scanner.hasNext()) {
+				this.memory[address] = scanner.nextShort(16);
+				address++;
+			}
+			scanner.close();
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
 }
