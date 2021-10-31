50f0300a6479b6ee1ba8ed032b4d44730723662b
김영서 <79343830+kimyeongseo@users.noreply.github.com>
2021-10-31 21:31:56 +0900

    Update Loader.java

--- Computer/src/Loader.java
+++ Computer/src/Loader.java
@@ -1,3 +1,22 @@
 public class Loader {
-
+		
+	static final short sizeHeader = 8; // 2
+	static final short indexPC = 0; 
+	static final short indexSP = 1; 
+		
+	private short startAddress = 0;
+	private short sizeData, sizeCode;
+		
+	public void loadHeader() {
+		Memory memory = new Memory();
+		CPU cpu = new CPU();
+		this.sizeData = 24; // 6
+		this.sizeCode = 192; // 48
+			
+		this.startAddress = memory.allocate((short)(sizeHeader/4 + this.sizeData/4 + this.sizeCode/4));
+		cpu.setPC((short) (startAddress + sizeHeader/4));
+		cpu.setSP((short) (startAddress + sizeHeader/4 + this.sizeCode/4 ));
+	}
+	
+	
 }
